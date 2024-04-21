f8524812e4d6d41133686bacb42afbd192105eb1
Phillipp Bertram <me@phillippbertram.de>
2024-04-21 16:00:11 +0200

    add loading spinner, better sorting

--- cmd/find.go
+++ cmd/find.go
@@ -2,27 +2,43 @@ package cmd
 
 import (
 	"fmt"
-	"os"
+	"time"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/briandowns/spinner"
 
 	"github.com/spf13/cobra"
 	"phillipp.io/go-xc-strings/internal"
 )
 
 var findCmd = &cobra.Command{
-	Use:   "find",
+	Use:   "find -s <Localizable.strings> [-d <path to swift code>] [-i <ignore pattern>...]",
 	Short: "Finds unused keys in .strings files",
-	Long: `This command checks for localization keys defined in a .strings file that are 
-not used in any Swift file within a specified directory.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		if stringsPath == "" || swiftDirectory == "" {
-			fmt.Println("Please specify both the strings file path and the Swift files directory.")
-			os.Exit(1)
+	Long: heredoc.Doc(
+		`Check for localization keys defined in a .strings file that are not used in any Swift file within a specified directory.`),
+	Example: heredoc.Doc(`
+		find -s Localizable.strings
+		find -s Localizable.strings -d Sources/MyApp -i "Pods/*" "Carthage/*" "*.generated.swift"
+	`),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if swiftDirectory == "" {
+			swiftDirectory = "."
 		}
+		if stringsPath == "" {
+			return fmt.Errorf("please specify the path to the .strings file and the directory containing Swift files")
+		}
+
+		// Start a spinner
+		s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+		s.Suffix = " Searching for unused keys..."
+		s.Start()
+
 		unusedKeys, err := internal.FindUnusedKeys(stringsPath, swiftDirectory, ignorePatterns)
+		s.Stop()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error finding unused keys: %v\n", err)
-			os.Exit(1)
+			return fmt.Errorf("error finding unused keys: %v", err)
 		}
+
 		if len(unusedKeys) > 0 {
 			fmt.Println("The following keys are unused:")
 			for _, key := range unusedKeys {
@@ -31,12 +47,14 @@ not used in any Swift file within a specified directory.`,
 		} else {
 			fmt.Println("No unused keys found.")
 		}
+
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(findCmd)
 	findCmd.PersistentFlags().StringVarP(&stringsPath, "strings", "s", "", "Path to the Localizable.strings file (required)")
-	findCmd.PersistentFlags().StringVarP(&swiftDirectory, "swift-dir", "d", "", "Path to the directory containing Swift files (required)")
+	findCmd.PersistentFlags().StringVarP(&swiftDirectory, "swift-dir", "d", "", "Path to the directory containing Swift files (.)")
 	findCmd.PersistentFlags().StringSliceVarP(&ignorePatterns, "ignore", "i", []string{}, "Glob patterns for files or directories to ignore")
 }
--- cmd/sort.go
+++ cmd/sort.go
@@ -3,31 +3,61 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 	"phillipp.io/go-xc-strings/internal"
 )
 
 var sortCmd = &cobra.Command{
-	Use:   "sort",
+	Use:   "sort [path]",
 	Short: "Sorts and groups keys in .strings files",
-	Long: `Sorts keys alphabetically in a .strings file and groups them by prefix. 
-	Keys with the same prefix are grouped together, and an empty line is added between different groups.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		if stringsPath == "" {
-			fmt.Println("Please specify the path to the .strings file.")
-			os.Exit(1)
+	Long: `Sorts keys alphabetically in a .strings file and groups them by prefix.
+If a directory path is provided, it sorts all .strings files within that directory.
+If a file path is provided, it sorts that specific file.`,
+	Example: heredoc.Doc(`
+		# sort all .strings files in the current directory and its subdirectories
+		sort
+
+		# sort all .strings files in a specific directory
+		sort path/to/directory
+
+		# sort a specific .strings file
+		sort Localizable.strings
+	`),
+	Args: cobra.MaximumNArgs(1), // Allows zero or one argument only
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Determine the path to sort
+		var path string
+		if len(args) == 1 {
+			path = args[0]
+		} else {
+			// If no argument is provided, use the current directory
+			var err error
+			path, err = os.Getwd()
+			if err != nil {
+				return fmt.Errorf("error getting current directory: %w", err)
+			}
 		}
-		err := internal.SortStringsFile(stringsPath)
+
+		// Make sure the path is absolute
+		absPath, err := filepath.Abs(path)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error sorting .strings file: %v\n", err)
-			os.Exit(1)
+			return fmt.Errorf("error resolving path: %w", err)
 		}
-		fmt.Println("The .strings file has been sorted and grouped successfully.")
+
+		// Sort the files
+		err = internal.SortStringsFiles(absPath)
+		if err != nil {
+			return fmt.Errorf("error sorting .strings files: %w", err)
+		}
+
+		fmt.Println("Sorting completed successfully.")
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(sortCmd)
-	sortCmd.PersistentFlags().StringVarP(&stringsPath, "strings", "s", "", "Path to the Localizable.strings file (required)")
 }
